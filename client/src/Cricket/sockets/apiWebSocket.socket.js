2d1cf634a2933e54e27fc6f09d5a3f93c0ec5bf8
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-11-28 15:59:54 +0100

    Le serveur broadcast les messages reçus aux clients

--- client/src/Cricket/sockets/apiWebSocket.socket.js
+++ client/src/Cricket/sockets/apiWebSocket.socket.js
@@ -1,6 +1,10 @@
 const host = import.meta.env.VITE_COOL_DARTS_HOST.replace(/^http/, "ws");
 const socket = new WebSocket(`${host}/api/web-socket`);
 
+socket.onmessage = function (event) {
+  console.log("WebSocket message received:", event.data);
+};
+
 export const apiWebSocket = {
   send(action) {
     socket.send(JSON.stringify(action));
--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const expressWs = require("express-ws")(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 33290;
 
 app
   .use(express.static(path.join(__dirname, "./client/dist")))
   .ws("/api/web-socket", (ws) => {
-    log("Client connected ");
-    ws.on("message", (msg) => log(`Socket received ${msg}`));
+    log("Client connected. Total clients: " + expressWs.getWss().clients.size);
+
+    ws.on("message", (msg) => {
+      log(`Socket received ${msg}`);
+      [...expressWs.getWss().clients]
+        .filter((aWs) => aWs !== ws)
+        .forEach((aWs) => aWs.send(msg));
+    });
   })
   .listen(port, () => console.log(`Listening on ${port}`));
 
