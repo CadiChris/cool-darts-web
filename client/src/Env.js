b6689a87b64068b51b480cb5f67d6c7f869e4576
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-11-30 22:30:25 +0100

    Réparer la constante

--- client/src/Env.js
+++ client/src/Env.js
@@ -1,3 +1,3 @@
 export const Env = {
-  COOL_DARTS_HOS: import.meta.env.VITE_COOL_DARTS_HOST.replace(/^http/, "ws"),
+  COOL_DARTS_HOST: import.meta.env.VITE_COOL_DARTS_HOST.replace(/^http/, "ws"),
 };
