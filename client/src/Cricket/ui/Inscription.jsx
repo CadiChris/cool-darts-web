dbd75b427444d09b9bcd55245e77f96659d6d625
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-12-04 13:53:34 +0100

    Améliorer l'affichage

--- client/src/Cricket/ui/Inscription.jsx
+++ client/src/Cricket/ui/Inscription.jsx
@@ -1,32 +1,37 @@
 import { useState } from "react";
+import "./Inscription.css";
 
 export function Inscription({ onInscription, joueurs, onDemarrerPartie }) {
   const [nom, setNom] = useState("");
 
   return (
-    <div>
+    <div className="inscription">
       <h1>Cool Darts </h1>
 
-      <input
-        type="text"
-        value={nom}
-        placeholder="Joueur"
-        onChange={(e) => setNom(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          onInscription(nom);
-          setNom("");
-        }}
-      >
-        Ajouter
-      </button>
+      <div className="inscrit">
+        <input
+          type="text"
+          value={nom}
+          placeholder="Joueur"
+          onChange={(e) => setNom(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            onInscription(nom);
+            setNom("");
+          }}
+        >
+          Ajouter
+        </button>
+      </div>
 
-      {joueurs.map(({ nom }) => (
-        <div key={nom}>{nom}</div>
-      ))}
+      <div className="joueurs">
+        {joueurs.map(({ nom }) => (
+            <div key={nom}>{nom}</div>
+        ))}
+      </div>
 
-      <div>
+      <div className="inscrire">
         <button onClick={onDemarrerPartie}>Démarrer</button>
       </div>
     </div>
