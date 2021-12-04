ae1a776e0b24d5c7f1075324c45fae8a3b6e7979
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2021-12-04 16:03:03 +0100

    Améliorer l'affichage

--- client/src/Cricket/ui/Commandes.jsx
+++ client/src/Cricket/ui/Commandes.jsx
@@ -2,17 +2,27 @@ export function Commandes({ touches, onReset, onSubmit }) {
   const aucuneTouche = touches.touches.length === 0;
   return (
     <div className="commandes">
-      <button className="bouton-principal" onClick={onReset}>❌</button>
+      <button className="bouton-principal" onClick={onReset}>
+        ❌
+      </button>
 
       <div className="resume">
         {touches.touches.map((t) => (
           <div key={t.chiffre}>
-            {t.nombre} x {t.chiffre === 25 ? "BULL" : t.chiffre}
+            <span>{t.nombre}</span> &nbsp;
+            <span className="font-legere">x</span> &nbsp;
+            <span className="font-epaisse">
+              {t.chiffre === 25 ? "BULL" : t.chiffre}
+            </span>
           </div>
         ))}
       </div>
 
-      <button className="bouton-principal" disabled={aucuneTouche} onClick={onSubmit}>
+      <button
+        className="bouton-principal"
+        disabled={aucuneTouche}
+        onClick={onSubmit}
+      >
         ✅
       </button>
     </div>
